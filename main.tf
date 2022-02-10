bb3061a9c6fa4e467ef159eadd50fe2cf7e2f924
Moses Alade <malade1@hawk.iit.edu>
2022-02-09 23:58:55 -0600

    declared s3 buckte modules included with logging encryption and versioning

--- main.tf
+++ main.tf
@@ -1,3 +1,24 @@
 provider "aws" {
   region = "us-east-1"
 }
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+  bucket = "well-s3-bucket"
+  acl    = "private"
+  versioning = {
+    enabled = var.versioning
+  }
+  logging = {
+    target_bucket = var.target_bucket
+    target_prefix = var.target_prefix
+  }
+  server_side_encryption_configuration = {
+    "rule" = {
+      "apply_server_side_encryption_by_default" = {
+        kms_master_key_id = var.kms_master_key_id
+        sse_algorithm     = var.sse_algorithm
+      }
+    }
+  }
+  tags = var.tags
+}
