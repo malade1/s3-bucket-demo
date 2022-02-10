47e167647784456c88f3ef52b94ad67fdaca1fce
Moses Alade <malade1@hawk.iit.edu>
2022-02-10 00:34:25 -0600

    updated s3 test env

--- s3-test/test.py
+++ s3-test/test.py
@@ -1,16 +1,16 @@
 import boto3
 from botocore.exceptions import ClientError
 from botocore.client import Config
-import AUTH
+import env
 import json
 
-aws_access_key_id = AUTH.AWS_ACCESS_KEY_ID
-aws_secret_access_key = AUTH.AWS_SECRET_ACCESS_KEY
+aws_access_key_id = env.AWS_ACCESS_KEY_ID
+aws_secret_access_key = env.AWS_SECRET_ACCESS_KEY
 
 session = boto3.Session(
     aws_access_key_id=aws_access_key_id,
     aws_secret_access_key=aws_secret_access_key,
-    region_name = AUTH.AWS_REGION
+    region_name = env.AWS_REGION
 )
 
 s3 = session.resource('s3')
